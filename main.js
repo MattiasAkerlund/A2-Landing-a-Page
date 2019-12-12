903d970d8af9f7583d44a7cddcb2e74657528784
Mattias Åkerlund <52895958+MattiasAkerlund@users.noreply.github.com>
2019-12-12 13:48:12 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+
+	$(document).ready(function(){
+		// set initially button state hidden
+		$("#btn").hide();
+		// use keyup event on email field
+		$("#email").keyup(function(){
+			if(validateEmail()){
+				// if the email is validated
+				// set input email border green
+				$("#email").css("border","2px solid green");
+				// and set label 
+				$("#emailMsg").html("<p class='text-success'>Validated</p>");
+			}else{
+				// if the email is not validated
+				// set border red
+				$("#email").css("border","2px solid red");
+				$("#emailMsg").html("<p class='text-danger'>Un-validated</p>");
+			}
+			buttonState();
+		});
+		// use keyup event on password
+		$("#pass").keyup(function(){
+			// check
+			if(validatePassword()){
+				// set input password border green
+				$("#pass").css("border","2px solid green");
+				//set passMsg 
+				$("#passMsg").html("<p class='text-success'>Password validated</p>");
+			}else{
+					// set input password border red
+				$("#pass").css("border","2px solid red");
+				//set passMsg 
+				$("#passMsg").html("<p class='text-danger'>Password not valid</p>");
+			}
+			buttonState();
+		});
+	});
+	function buttonState(){
+		if(validateEmail() && validatePassword()){
+		
+			$("#btn").show();
+		}else{
+			
+			$("#btn").hide();
+		}
+	}
+	function validateEmail(){
+		// get value of input email
+		var email=$("#email").val();
+		// use reular expression
+		 var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+		 if(reg.test(email)){
+		 	return true;
+		 }else{
+		 	return false;
+		 }
+	}
+	function validatePassword(){
+		//get input password value
+		var pass=$("#pass").val();
+		// check it s length
+		if(pass.length > 7){
+			return true;
+		}else{
+			return false;
+		}
+	}
